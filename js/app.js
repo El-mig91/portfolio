0b2e4988a3bfb0959f79285f7c1e20b6b43638e0
Miguel Nongni <miguel.nongni85@gmail.com>
2026-03-11 23:03:43 +0100

    Revert "Add files via upload"
    This reverts commit 6aa0f57672db05f6294e3e2fce01fc65953fff36.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const projectCards = document.querySelectorAll('.project-card');
 
-    projectCards.forEach(card => {
-        card.addEventListener('click', () => {
-            card.classList.toggle('flipped');
-        });
-    });
-});
