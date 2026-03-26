7c8bc8667c36dc4ef725bcd7cd15c87b72a39f22
Miguel Nongni <miguel.nongni85@gmail.com>
2026-03-26 11:41:23 +0100

    ajout de cv ,correction de la page conatct

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const projectCards = document.querySelectorAll('.project-card');
 
+    projectCards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('flipped');
+        });
+    });
+});
